f026b7fb7cceec1db6bbbc82aeeec0273f6063ea
Schubert Santos <schubert.asantos>
2024-12-05 17:51:00 -0300

    Inserindo comenários para melhor entendimento do código

--- scripts.js
+++ scripts.js
@@ -1,16 +1,23 @@
-let botao = document.getElementById("botao")
-let input = document.getElementById("input")
-let origem = document.getElementById("origem")
-let destino = document.getElementById("destino")
+// Seleciona os elementos HTML
+let botao = document.getElementById("botao");
+let input = document.getElementById("input");
+let origem = document.getElementById("origem");
+let destino = document.getElementById("destino");
 
-botao.addEventListener("click", converter)
+// Adiciona um evento de clique ao botão
+botao.addEventListener("click", converter);
 
+// Função para converter a moeda
 function converter() {
-    if(origem.value == 'real' && destino.value == 'dolar') {
-        alert(input.value / 5.8)
-    } else if (origem.value == 'dolar' && destino.value == 'real' ) {
-        alert(input.value * 5.8)
-    } else if (origem.value == destino.value) {
-        alert(input.value)
-    }
+  // Verifica se a moeda de origem é Real e a moeda de destino é Dólar
+  if (origem.value == 'real' && destino.value == 'dolar') {
+    // Converte Reais para Dólares
+    alert(input.value / 5.8);
+  } else if (origem.value == 'dolar' && destino.value == 'real') {
+    // Converte Dólares para Reais
+    alert(input.value * 5.8);
+  } else if (origem.value == destino.value) {
+    // Se as moedas são iguais, exibe o mesmo valor
+    alert(input.value);
+  }
 }
